1edb48555bd97f18787bd216617c0747cc17679a
athiyyah riqqah <athiyyah@mhs.unsyiah.ac.id>
2020-12-28 07:51:17 +0700

    router

--- lib/app/router.dart
+++ lib/app/router.dart
@@ -3,7 +3,9 @@ import 'package:auto_route/auto_route_annotations.dart';
 import '../ui/views/auth/auth_view.dart';
 import '../ui/views/forgot/forgot_view.dart';
 import '../ui/views/home/home_view.dart';
+import '../ui/views/join_room/join_room_view.dart';
 import '../ui/views/register/register_view.dart';
+import '../ui/views/room/room_view.dart';
 
 @MaterialAutoRouter(
   routes: <AutoRoute>[
@@ -11,6 +13,8 @@ import '../ui/views/register/register_view.dart';
     MaterialRoute(page: RegisterView),
     MaterialRoute(page: ForgotView),
     MaterialRoute(page: HomeView),
+    MaterialRoute(page: JoinRoomView),
+    MaterialRoute(page: RoomView),
   ],
 )
 class $Router {}
