b7a78024364156150cea6c822e32f3c3a0873412
athiyyah riqqah <athiyyah@mhs.unsyiah.ac.id>
2020-12-28 07:49:38 +0700

    main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:stacked_services/stacked_services.dart';
 
@@ -12,9 +13,34 @@ void main() {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    return FutureBuilder(
+      // Initialize FlutterFire
+      future: Firebase.initializeApp(),
+      builder: (context, snapshot) {
+        // Check for errors
+        if (snapshot.hasError) {
+          return Center(
+            child: Container(
+              child: Text('error'),
+            ),
+          );
+        }
+
+        // Once complete, show your application
+        if (snapshot.connectionState == ConnectionState.done) {
+          return studentMonitoringApp();
+        }
+
+        // Otherwise, show something whilst waiting for initialization to complete
+        return CircularProgressIndicator();
+      },
+    );
+  }
+
+  MaterialApp studentMonitoringApp() {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Bintex Driver',
+      title: 'Student Monitoring',
       initialRoute: rt.Routes.authView,
       onGenerateRoute: rt.Router().onGenerateRoute,
       navigatorKey: locator<NavigationService>().navigatorKey,
